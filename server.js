87cb6aa68c850bf38df56e4850e5e6d9551a0a49
neilsen zulueta <neilzulueta@gmail.com>
2023-12-03 01:30:54 -0800

    updated PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
 const savedNotes = require('./Develop/db/db.json');
 
