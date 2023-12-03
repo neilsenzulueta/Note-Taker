d42c6de28642946dd530583e7f90d50d287fdaa3
neilsen zulueta <neilzulueta@gmail.com>
2023-12-02 22:13:12 -0800

    udpated server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const PORT = 8080
 
 app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
